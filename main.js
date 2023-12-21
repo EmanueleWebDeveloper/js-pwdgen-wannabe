b4c970c0f7738e6562d6127fbb45d75c9dc4e036
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2023-12-21 16:57:52 +0100

    finish

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-let nomeUtente = prompt( "dammi il tuo nome:" );
-let cognomeUtente = prompt( "dammi il tuo cognome:");
-let colorePreferito = prompt( "dammi il tuo colore preferito:");
+function submitForm() {
+    // Ottenere i valori inseriti dall'utente
+    let nome = document.getElementById('nome').value;
+    let cognome = document.getElementById('cognome').value;
+    let colorePreferito = document.getElementById('colorePreferito').value;
 
-console.log( nomeUtente + cognomeUtente + colorePreferito );
-document.writeln( nomeUtente + cognomeUtente + colorePreferito );
+    
+    console.log("Nome:", nome);
+    console.log("Cognome:", cognome);
+    console.log("Colore preferito:", colorePreferito);
 
+ 
+    let risultato = nome + cognome + colorePreferito + '23';
+
+
+    console.log("Risultato:", risultato);
+
+    document.getElementById('risultato').innerText = risultato;
+}
