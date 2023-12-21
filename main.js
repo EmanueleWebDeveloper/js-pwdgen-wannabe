622d9215ce6c75fedf4bcc483d75e94c35aef4a3
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2023-12-21 16:31:01 +0100

    esercizio

---
+++ main.js
@@ -0,0 +1,7 @@
+let nomeUtente = prompt( "dammi il tuo nome:" );
+let cognomeUtente = prompt( "dammi il tuo cognome:");
+let colorePreferito = prompt( "dammi il tuo colore preferito:");
+
+console.log( nomeUtente + cognomeUtente + colorePreferito );
+document.writeln( nomeUtente + cognomeUtente + colorePreferito );
+
